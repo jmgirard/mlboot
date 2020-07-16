0f9f87e2ff08ea3b9790240c50a1098febf2fac0
jmgirard <me@jmgirard.com>
2020-07-16 09:54:36 -0400

    Add correlation score and na.rm argument to all metrics

--- R/metrics.R
+++ R/metrics.R
@@ -2,19 +2,19 @@
 
 # Accuracy / Agreement
 #' @export
-accuracy_score <- function(y_true, y_pred) {
+accuracy_score <- function(y_true, y_pred, na.rm = TRUE) {
   stopifnot(length(y_true) == length(y_pred))
-  mean(y_true == y_pred)
+  mean(y_true == y_pred, na.rm = na.rm)
 }
 
 # F1 Score
 #' @export
-f1_score <- function(y_true, y_pred, pos = 1, neg = 0) {
+f1_score <- function(y_true, y_pred, pos = 1, neg = 0, na.rm = TRUE) {
   stopifnot(length(y_true) == length(y_pred))
-  tp <- sum(y_true == pos & y_pred == pos)
-  fp <- sum(y_true == neg & y_pred == pos)
-  fn <- sum(y_true == pos & y_pred == neg)
-  tn <- sum(y_true == neg & y_pred == neg)
+  tp <- sum(y_true == pos & y_pred == pos, na.rm = na.rm)
+  fp <- sum(y_true == neg & y_pred == pos, na.rm = na.rm)
+  fn <- sum(y_true == pos & y_pred == neg, na.rm = na.rm)
+  tn <- sum(y_true == neg & y_pred == neg, na.rm = na.rm)
   precision <- tp / (tp + fp)
   recall <- tp / (tp + fn)
   2 * precision * recall / (precision + recall)
@@ -22,12 +22,12 @@ f1_score <- function(y_true, y_pred, pos = 1, neg = 0) {
 
 # Matthews Correlation Coefficient
 #' @export
-matthews_corrcoef <- function(y_true, y_pred, pos = 1, neg = 0) {
+matthews_corrcoef <- function(y_true, y_pred, pos = 1, neg = 0, na.rm = TRUE) {
   stopifnot(length(y_true) == length(y_pred))
-  tp <- sum(y_true == pos & y_pred == pos)
-  fp <- sum(y_true == neg & y_pred == pos)
-  fn <- sum(y_true == pos & y_pred == neg)
-  tn <- sum(y_true == neg & y_pred == neg)
+  tp <- sum(y_true == pos & y_pred == pos, na.rm = na.rm)
+  fp <- sum(y_true == neg & y_pred == pos, na.rm = na.rm)
+  fn <- sum(y_true == pos & y_pred == neg, na.rm = na.rm)
+  tn <- sum(y_true == neg & y_pred == neg, na.rm = na.rm)
   (tp * tn - fp * fn) / sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
 }
 
@@ -35,28 +35,40 @@ matthews_corrcoef <- function(y_true, y_pred, pos = 1, neg = 0) {
 
 # Explained Variance Score
 #' @export
-explained_variance_score <- function(y_true, y_pred) {
+explained_variance_score <- function(y_true, y_pred, na.rm = TRUE) {
   stopifnot(length(y_true) == length(y_pred))
-  1 - var(y_true - y_pred) / var(y_true)
+  1 - var(y_true - y_pred, na.rm = na.rm) / var(y_true, na.rm = na.rm)
 }
 
 # Mean Absolute Error
 #' @export
-mean_absolute_error <- function(y_true, y_pred) {
+mean_absolute_error <- function(y_true, y_pred, na.rm = TRUE) {
   stopifnot(length(y_true) == length(y_pred))
-  mean(abs(y_true - y_pred))
+  mean(abs(y_true - y_pred), na.rm = na.rm)
 }
 
 # Mean Squared Error
 #' @export
-mean_squared_error <- function(y_true, y_pred) {
+mean_squared_error <- function(y_true, y_pred, na.rm = TRUE) {
   stopifnot(length(y_true) == length(y_pred))
-  mean((y_true - y_pred)^2)
+  mean((y_true - y_pred)^2, na.rm = na.rm)
 }
 
 # R-squared / Coefficient of Determination
 #' @export
-r2_score <- function(y_true, y_pred) {
+r2_score <- function(y_true, y_pred, na.rm = TRUE) {
   stopifnot(length(y_true) == length(y_pred))
-  1 - sum((y_true - y_pred)^2) / sum((y_true - mean(y_pred))^2)
+  1 - sum((y_true - y_pred)^2, na.rm = na.rm) / 
+    sum((y_true - mean(y_pred, na.rm = na.rm))^2, na.rm = na.rm)
 }
+
+# Pearson Correlation
+#' @export
+cor_score <- function(y_true, y_pred, na.rm = TRUE) {
+  stopifnot(length(y_true) == length(y_pred))
+  use <- ifelse(na.rm == TRUE, "pairwise", "everything")
+  cor(y_pred, y_true, use = use, method = "pearson")
+}
+
+
+
